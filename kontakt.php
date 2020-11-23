22a0b757321db5b8ec7f2c9b646064ca51f7bdba
unknown <paparazzit@gmail.com>
2020-11-23 16:49:00 +0100

    klijenti

--- kontakt.php
+++ kontakt.php
@@ -54,37 +54,6 @@
     
     </section>
     
-    <!-- nasi klijenti -->
-    <section class="nasiKlijenti">
-        <hgroup class="text-center">
-          <h2 >Naši klijenti</h2>
-          <p>Vivamus ante est, tempus eget suscipit sit amet, laoreet a dui. Fusce 
-            aliquet arcu eget <br>
-             sapien varius fringilla. Suspendisse porttitor efficitur facilisis.</p>
-        </hgroup>
-        
-        <div class="container">
-          <div class="row flex-wrap justify-content-center align-items-center">
-            
-            <div class="col-lg-2 col-sm-5 text-center">
-              <img src="img/nasi_klijenti_1.png " alt="" class="img-fluid mr-2">
-              
-            </div>
-  
-            <div class="col-lg-2  col-sm-5 text-center">
-              <img src="img/nasi_klijenti_2.png" alt="" class="img-fluid mr-2">
-              
-            </div>
-            <div class="col-lg-2  col-sm-5 text-center">
-              <img src="img/nasi_klijenti_3.png" alt="" class="img-fluid mr-2">
-              
-            </div>
-            <div class="col-lg-2  col-sm-5 text-center">
-              <img src="img/nasi_klijenti_4.png" alt="" class="img-fluid">
-              
-            </div>
-          </div>
-        </div>
-      </section>
+    <?php include 'klijenti.php' ?>
     <!-- FOOTER -->
 <?php include 'footer.php' ?>
